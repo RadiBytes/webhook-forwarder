0b95017f7ca0932ed797e734a1c91a1c9016b33c
Radi <evaradi18@gmail.com>
2022-07-07 12:57:15 +0100

    bugfix

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from src.webhook import webhook
+from src.config import DEBUG
 
 #--------- Optional Settings ------#
 # Add subdirectory for your webhook URL. Default is empty string
 webhook.add_subdir = ""
-
-# Set debug mode. Default is True
-webhook.debug = False
 
 # set to "hub.challenge" for whatsapp api webhooksetup
 # otherwise, leave empty. Default is empty
--- src/webhook.py
+++ src/webhook.py
@@ -4,7 +4,6 @@ import json
 import requests
 from flask import Flask, request
 from waitress import serve
-from .config import SUBPATH, DEBUG
 
 
 IS_WHATSAPP = True
@@ -18,23 +17,23 @@ class _Webhook_obj:
 
     def __init__(self) -> None:
         self._webhook_forward_url: str = ''
-        self._subdir: str = SUBPATH
+        self._subpath: str = ''
         # set to "hub.challenge" for whatsapp api webhooksetup
         self.verify_query_key: str = "hub.challenge" if IS_WHATSAPP else ''
-        self.debug = DEBUG
+        self.debug = True
 
-    def add_subdir(self, text: str):
-        self._subdir = text.strip('/')
+    def add_subpath(self, text: str):
+        self._subpath = text.strip('/')
 
     def run(self):
         if self.debug:
             print(f"""***** Running Webhook Forwarder *****
-        Receivning at: /{self._subdir}
+        Receivning at: /{self._subpath}
         Forwarding to: {self._webhook_forward_url}""")
             app.run(debug=self.debug, host="0.0.0.0", port=os.getenv("PORT"))
         else:
             print(f"""***** Running Webhook Forwarder *****
-        Receivning at: /{self._subdir}
+        Receivning at: /{self._subpath}
         Forwarding to: {self._webhook_forward_url}""")
         serve(app, host='0.0.0.0', port=os.getenv(
             "PORT") if os.getenv("PORT") else 80)
@@ -44,7 +43,7 @@ webhook = _Webhook_obj()
 # '/'->(post) endpoint to receive webhook from wa
 
 
-@app.route('/'+webhook._subdir, methods=['POST', 'GET'])
+@app.route('/'+webhook._subpath, methods=['POST', 'GET'])
 def forward_message():
     # send response to saved webhook
     if request.method == "POST":
@@ -86,12 +85,12 @@ def set_webhook():
 if __name__ == "__main__":
     if webhook.debug:
         print(f"""***** Running Webhook Forwarder *****
-        Receivning at: /{webhook._subdir}
+        Receivning at: /{webhook._subpath}
         Forwarding to: {webhook._webhook_forward_url}""")
         app.run(debug=webhook.debug, host="0.0.0.0", port=os.getenv("PORT"))
     else:
         print(f"""***** Running Webhook Forwarder *****
-        Receivning at: /{webhook._subdir}
+        Receivning at: /{webhook._subpath}
         Forwarding to: {webhook._webhook_forward_url}""")
         serve(app, host='0.0.0.0', port=os.getenv(
             "PORT") if os.getenv("PORT") else 80)
