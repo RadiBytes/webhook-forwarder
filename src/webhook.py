19ab4120d975ff65dea8329d7b78656e89083924
Radi <evaradi18@gmail.com>
2024-10-24 10:36:40 +0100

    bugfix

--- src/webhook.py
+++ src/webhook.py
@@ -1,6 +1,6 @@
-# setup flask
+"""setup flask"""
+
 import os
-import json
 import requests
 from flask import Flask, request
 from waitress import serve
@@ -16,7 +16,7 @@ class _Webhook_obj:
     '''Initialize webhook settings'''
 
     def __init__(self) -> None:
-        self._webhook_forward_url: str = ''
+        self.webhook_forward_url: str = ""
         self._subpath: str = ''
         # set to "hub.challenge" for whatsapp api webhooksetup
         self.verify_query_key: str = "hub.challenge" if IS_WHATSAPP else ''
@@ -27,14 +27,18 @@ class _Webhook_obj:
 
     def run(self):
         if self.debug:
-            print(f"""***** Running Webhook Forwarder *****
+            print(
+                f"""***** Running Webhook Forwarder *****
         Receivning at: /{self._subpath}
-        Forwarding to: {self._webhook_forward_url}""")
+        Forwarding to: {self.webhook_forward_url}"""
+            )
             app.run(debug=self.debug, host="0.0.0.0", port=os.getenv("PORT"))
         else:
-            print(f"""***** Running Webhook Forwarder *****
+            print(
+                f"""***** Running Webhook Forwarder *****
         Receivning at: /{self._subpath}
-        Forwarding to: {self._webhook_forward_url}""")
+        Forwarding to: {self.webhook_forward_url}"""
+            )
         serve(app, host='0.0.0.0', port=os.getenv(
             "PORT") if os.getenv("PORT") else 80)
 
@@ -50,9 +54,9 @@ def forward_message():
         update = request.data.decode('utf-8')  # .get_json(force=True)
         args = update
         try:
-            
-            print("sent to", webhook._webhook_forward_url)
-            return requests.post(webhook._webhook_forward_url, data=update).textexcept:
+            print("sent to", webhook.webhook_forward_url)
+            return requests.post(webhook.webhook_forward_url, data=update).text
+        except:
             print("Forward not successful")
     else:
         try:
@@ -77,8 +81,8 @@ def forward_webhook():
         # update = json.loads(update)
 
         try:
-            requests.post(webhook._webhook_forward_url+"/webhook", data=update).text
-            print("sent to", webhook._webhook_forward_url)
+            requests.post(webhook.webhook_forward_url + "/webhook", data=update).text
+            print("sent to", webhook.webhook_forward_url)
             return f"success: forwarded", 200
         except:
             print("Forward not successful")
@@ -95,7 +99,7 @@ def forward_webhook():
 
 @app.route('/delete_webhook')
 def delete_webhook():
-    webhook._webhook_forward_url = ""
+    webhook.webhook_forward_url = ""
     return "success", 200
 
 
@@ -104,25 +108,29 @@ def set_webhook():
     if request.method == "POST":
         update = request.get_json(force=True)
         try:
-            _webhook_forward_url = update["webhook_url"]
-            webhook._webhook_forward_url = _webhook_forward_url
-            print(f"set webhook success to {webhook._webhook_forward_url}")
-            return f"set webhook success to {webhook._webhook_forward_url}", 200
+            webhook_forward_url = update["webhook_url"]
+            webhook.webhook_forward_url = webhook_forward_url
+            print(f"set webhook success to {webhook.webhook_forward_url}")
+            return f"set webhook success to {webhook.webhook_forward_url}", 200
         except:
-            print("Could not set _webhook_forward_url")
-            return "Could not set _webhook_forward_url", 402
-    return f"webhook url is {webhook._webhook_forward_url}", 200
+            print("Could not set webhook_forward_url")
+            return "Could not set webhook_forward_url", 402
+    return f"webhook url is {webhook.webhook_forward_url}", 200
 
 
 if __name__ == "__main__":
     if webhook.debug:
-        print(f"""***** Running Webhook Forwarder *****
+        print(
+            f"""***** Running Webhook Forwarder *****
         Receivning at: /{webhook._subpath}
-        Forwarding to: {webhook._webhook_forward_url}""")
+        Forwarding to: {webhook.webhook_forward_url}"""
+        )
         app.run(debug=webhook.debug, host="0.0.0.0", port=os.getenv("PORT"))
     else:
-        print(f"""***** Running Webhook Forwarder *****
+        print(
+            f"""***** Running Webhook Forwarder *****
         Receivning at: /{webhook._subpath}
-        Forwarding to: {webhook._webhook_forward_url}""")
+        Forwarding to: {webhook.webhook_forward_url}"""
+        )
         serve(app, host='0.0.0.0', port=os.getenv(
             "PORT") if os.getenv("PORT") else 80)
